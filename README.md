ec4d07795c30e18cf0f8d83641fd2014d517265d
boris rafael beker <borisrafaelbeker@gmail.com>
2023-12-02 21:03:14 -0300

    inicializando repositorio

---
+++ README.md
@@ -0,0 +1,15 @@
+#PROYECTOFINAL PORTFOLIO BORIS RAFAEL BEKER
+>Un portfolio diseñado para mostrar mis proyectos futuros.
+
+## TECNOLOGÍAS
+- html
+- css
+- bootstrap
+- sass
+
+## SECCIONES
+- SOBRE MI
+- PROYECTOS
+- HABILIDADES
+- EDUCACION
+- CONTACTO
---
+++ main.js
@@ -0,0 +1,11 @@
+let number1 = parseInt(prompt("ingresa numero 1"));
+let number2 = parseInt(prompt("ingresa numero 2"));
+let number3 = parseInt(prompt("ingresa numero 3"));
+
+let resultado = number1 + number2;
+
+resultado = resultado * number3;
+
+let resultado2 = number2 / number3;
+
+console.log(resultado + "\n" + resultado2);
