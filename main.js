2595b6bd166e322b6aff6712dba02aec8e13ed99
boris rafael beker <borisrafaelbeker@gmail.com>
2023-12-02 21:56:51 -0300

    casi terminando primera entrega

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-let number1 = parseInt(prompt("ingresa numero 1"));
-let number2 = parseInt(prompt("ingresa numero 2"));
-let number3 = parseInt(prompt("ingresa numero 3"));
+let pedido = parseInt(prompt("Especifíque su pedido: \n 1 = Página Web \n 2 = Mantenimiento \n 3 = Contratación"));
+let precioBase = 500;
 
-let resultado = number1 + number2;
+function calcularPrecio(parametro){
+    if(parametro === 2){
+        return precioBase / 5;
+    } else if(parametro === 3){
+        return precioBase * 3;
+    }
+}
 
-resultado = resultado * number3;
-
-let resultado2 = number2 / number3;
-
-console.log(resultado + "\n" + resultado2);
+if(pedido === 1){
+    alert("Usted eligió: Página Web.")
+    alert("Su pedido vale $" + precioBase + ".")
+}else if(pedido === 2){
+    alert("Usted eligió: Mantenimiento.")
+    alert("Su pedido vale $" + calcularPrecio(pedido) + ".")
+}else if(pedido === 3){
+    alert("Usted eligió: Contratación.")
+    alert("El valor de la contratación es $" + calcularPrecio(pedido) + " al mes.")
+    let equipo = parseInt(prompt("Especifíque el número de integrantes de su equipo."));
+    for(i = 1; i <= equipo; i++){
+        let nombre = prompt("Diga el nombre del integrante " + i)
+        alert("Saludos a " + nombre + ".\nSerá un placer trabajar con usted.")
+    }
+}else {
+    alert("No eligió ningún pedido.")
+}
